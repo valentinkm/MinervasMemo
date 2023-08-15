35c94e47acfea072e64a29b330de88336fc049f9
valentinkm <valentin.kriegmair@student.hu-berlin.de>
2023-08-15 21:01:24 +0200

    speaker split and blind split

--- summarizer.py
+++ summarizer.py
@@ -11,28 +11,51 @@ from langchain.chains.summarize import load_summarize_chain
 from langchain.docstore.document import Document
 from langchain.prompts import PromptTemplate
 from langchain.text_splitter import RecursiveCharacterTextSplitter, CharacterTextSplitter
+from langchain.text_splitter import MarkdownHeaderTextSplitter
 
-def summarize_transcript(vtt_file_path):
-    md_path = vtt_to_md(vtt_file_path)
-    loader = UnstructuredMarkdownLoader(md_path)
-    data = loader.load()
-    return data
+md_path = vtt_to_md("docs/Formal Methods Standup-20230802 0800-1.vtt")
 
-data = summarize_transcript("docs/Formal Methods Standup-20230802 0800-1.vtt")
-
-txt = ' '.join([d.page_content for d in docs])
-
+# Recursive splitting to consider different separators in generic text
 r_splitter = RecursiveCharacterTextSplitter(
     chunk_size=3000,
-    chunk_overlap=100, 
+    chunk_overlap=200, 
     separators=["\n\n", "\n", " ", ""],
     length_function = len
 )
 
-docs = r_splitter.split_text(txt)
+######################## Option 1 Context Aware Split by Speaker ################################
+
+with open(md_path, 'r', encoding='utf-8') as file:
+    md_script = file.read()
+
+headers_to_split_on = [("###", "Speaker 3")]
+
+md_splitter = MarkdownHeaderTextSplitter(headers_to_split_on=headers_to_split_on)
+
+speaker_splits = md_splitter.split_text(md_script)
+
+speaker_recursive_splits = r_splitter.split_documents(speaker_splits)
+
+
+########################### Option 2 Blind Splitting #################################
+
+def load_transcript(md_path):
+    loader = UnstructuredMarkdownLoader(md_path)
+    data = loader.load()
+    return data
+
+data = load_transcript(md_path)
+
+txt = ' '.join([d.page_content for d in data])
+
+blind_recursive_splits = r_splitter.split_documents(txt)
+
+################################################################################
 
 model = AzureChatOpenAI(request_timeout=30,
                                     model = "summarizer", 
                                     temperature=0.1, 
                                     deployment_name=os.getenv("OPENAI_MODEL_NAME"))
+
+
 

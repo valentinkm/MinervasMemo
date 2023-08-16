127b84a545288a804c454df788f691150ef83a68
valentinkm <valentin.kriegmair@student.hu-berlin.de>
2023-08-16 17:31:10 +0200

    add simple sequential chain for formatting

--- summarizer.py
+++ summarizer.py
@@ -116,23 +116,21 @@ map_reduce_chain = MapReduceDocumentsChain(
 # result = map_reduce_chain.run(docs)
 
 ############################ Summarize Option 2 Refine #################################################
-target_len = range(100, 250)
-
 
 from langchain.prompts import PromptTemplate
 
 prompt_template = """You are an excellent executive assistent. You are given an excerpt of a machine generated transcript of a stand-up meeting of the formal methods group at MPI-Berlin. The script sometimes contains words that do not fit the context or misspelled names and may need to be replaced accordingly to make sense.
     {text}
-    Based on the excerpt please identify all relevant talking points and agreed upon action items. Do NOT make any points up. For context: Regular participants of the meeting include: Aaron Peikert, Timo von Oertzen, Hannes Diemerling, Leonie Hagitte, Maximilian Ernst, Valentin Kriegmair, Leo Kosanke, Ulman Lindenberger, Moritz Ketzer and Nicklas Hafiz.
+    Based on the excerpt please identify all relevant talking points and agreed upon action items. Do NOT make any points up. For context: Team members regulalry attending the meeting include: Aaron Peikert, Timo von Oertzen, Hannes Diemerling, Leonie Hagitte, Maximilian Ernst, Valentin Kriegmair, Leo Kosanke, Ulman Lindenberger, Moritz Ketzer and Nicklas Hafiz.
     Tone: formal
     Format: 
-    - Concise meeting summary
+    - Concise and detailed meeting summary
     - Participants: <participants>
     - Discussed: <Discussed-items>
     - Follow-up actions: <a-list-of-follow-up-actions-with-owner-names>
     Tasks:
     - Highlight who is speaking, action items, dates and agreements
-    - Create a list of bullet points for each section by a speaker or the group.
+    - Step by step list all points of each speaker or group of speakers
     - Work step by step.
     CONCISE SUMMARY IN ENGLISH:"""
 
@@ -147,12 +145,12 @@ refine_template = (
         "------------\n"
         "{text}\n"
         "------------\n"
-        f"Given the new context, refine the original summary in English within {target_len} words: following the format"
-        "Participants: <participants>"
+        f"Given the new context, refine the original summary: following the format"
+        "Participants: <Participants>"
         "Discussed: <Discussed-items>"
         "Follow-up actions: <a-list-of-follow-up-actions-with-owner-names>"
         "Highlight who is speaking, action items, dates and agreements"
-        "Create a list of bullet points for each section by a speaker or the group."
+        "List points for each section by a speaker or the group in detail."
         "Work step by step."
         "If the context isn't useful, return the original summary, do NOT make anything up. Highlight agreements and follow-up actions and owners."
 )
@@ -170,7 +168,36 @@ refine_chain = load_summarize_chain(
         refine_prompt=refine_prompt,
 )
 
-refine_result = refine_chain({"input_documents": docs}, return_only_outputs=True)
+# refine_result = refine_chain({"input_documents": docs}, return_only_outputs=True)
 
-refine_result
+from langchain.chains import SimpleSequentialChain
+from langchain.prompts import ChatPromptTemplate
 
+
+prompt_bullet = ChatPromptTemplate.from_template(
+    "You are an excellent executive assistant.\
+    Your task is to create a detailed bulleted summary of the following meeting notes.\
+    Please format your response as markdown code. Highlight agreed upon actions \
+    Summary: {summary}\
+    Format: \
+    '## Meeting Summary \
+    ### Participants\
+    ### Discussed\
+    - **Speaker 1**\
+        - points \
+        - ...\
+    - ...\
+    ### Action Items\
+    - <a-list-of-follow-up-actions-with-owner-names>\
+    ### Side Comments'"
+)
+
+chain_bullet = LLMChain(llm=llm, prompt=prompt_bullet, 
+                     output_key="bullet-summary"
+                    )
+
+overall_simple_chain = SimpleSequentialChain(chains=[refine_chain, chain_bullet],
+                                             verbose=True
+                                            )
+
+overall_simple_chain({"input": docs}, return_only_outputs=False)

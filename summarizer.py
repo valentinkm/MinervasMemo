bdcebaeff6d588043e75e68995ba9d058675036f
valentinkm <valentin.kriegmair@student.hu-berlin.de>
2023-09-13 18:24:47 +0200

    integrate langfuse

--- summarizer.py
+++ summarizer.py
@@ -1,15 +1,22 @@
 # Purpose: To summarize the meeting transcript
 # Setup
+import os
 from dotenv import load_dotenv
 from langchain.chains.summarize import load_summarize_chain
 from langchain.chains.llm import LLMChain
 from langchain.chains import SimpleSequentialChain
 from prompts import init_proompt, refine_prompt, bullet_prompt
 from langchain.chat_models import ChatOpenAI
+from langfuse.callback import CallbackHandler
 
 load_dotenv()
 
-llm = ChatOpenAI(temperature=0.7, model_name="gpt-3.5-turbo")
+PUBLIC_KEY = os.getenv('PUBLIC_KEY')
+SECRET_KEY = os.getenv('SECRET_KEY')
+
+handler = CallbackHandler(PUBLIC_KEY, SECRET_KEY)
+
+llm = ChatOpenAI(temperature=0.5, model_name="gpt-3.5-turbo")
 
 
 refine_chain = load_summarize_chain(
@@ -31,5 +38,5 @@ overall_simple_chain = SimpleSequentialChain(chains=[refine_chain, bullet_chain]
                                             )
 
 def generate_summary(docs):
-    final_summary = overall_simple_chain({"input": docs}, return_only_outputs=True)
+    final_summary = overall_simple_chain({"input": docs}, return_only_outputs=True, callbacks=[handler])
     return final_summary['output']

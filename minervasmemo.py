b6d2d36594269cbe1fe405b3055074337debfff4
valentinkm <valentin.kriegmair@student.hu-berlin.de>
2023-09-21 02:10:30 +0200

    simplify modularization

--- minervasmemo.py
+++ minervasmemo.py
@@ -7,7 +7,7 @@ def main():
     parser = argparse.ArgumentParser(description="Convert a .vtt file to a final processed output")
     parser.add_argument("-i", "--input", required=True, help="Path to the .vtt file")
     parser.add_argument("-o", "--output", help="Base path to the output file(s)")
-    parser.add_argument("--mode", choices=['convert', 'summarize', 'all'], default='all', help="Operation mode")
+    parser.add_argument("--mode", choices=['convert', 'summarize'], default='convert', help="Operation mode")
 
     args = parser.parse_args()
 
@@ -15,15 +15,15 @@ def main():
     default_output_base = args.input.replace(".vtt", "")
     output_base = args.output if args.output else default_output_base
 
-    if args.mode in ['convert', 'all']:
+    if args.mode == 'convert':
         # Convert the .vtt file to a .md file
         convert_output = f"{output_base}_transcript.md"
+        vtt_to_md(args.input, convert_output)
+
+    elif args.mode == 'summarize':
+        # First, convert the .vtt file to a .md file
+        convert_output = f"{output_base}_transcript.md"
         raw_md = vtt_to_md(args.input, convert_output)
-    
-    if args.mode in ['summarize', 'all']:
-        # If in 'all' mode, re-read the raw .md file
-        if args.mode == 'all':
-            raw_md = convert_output
 
         # Split .md in chunks
         docs = split_transcript(raw_md)
@@ -31,10 +31,9 @@ def main():
         # Generate llm based summary
         summary_output = f"{output_base}_summary.md"
         summary_md = generate_summary(docs)
-
+        
         with open(summary_output, 'w') as file:
             file.write(summary_md)
 
 if __name__ == "__main__":
     main()
-    

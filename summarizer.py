ae505e5c70cda66202f8dcbc8c217325d18f7a68
valentinkm <valentin.kriegmair@student.hu-berlin.de>
2023-09-22 01:07:18 +0200

    adjusted summarizer for chatopenai model

--- summarizer.py
+++ summarizer.py
@@ -2,41 +2,51 @@
 # Setup
 import os
 from dotenv import load_dotenv
+from langchain.chat_models import ChatOpenAI
 from langchain.chains.summarize import load_summarize_chain
 from langchain.chains.llm import LLMChain
 from langchain.chains import SimpleSequentialChain
 from prompts import init_proompt, refine_prompt, bullet_prompt
-from langchain.chat_models import ChatOpenAI
 from langfuse.callback import CallbackHandler
 
-load_dotenv()
+# Initialization variables set to None
+llm = None
+refine_chain = None
+bullet_chain = None
+overall_simple_chain = None
+handler = None
 
-PUBLIC_KEY = os.getenv('PUBLIC_KEY')
-SECRET_KEY = os.getenv('SECRET_KEY')
+def initialize_summarizer():
+    global llm, refine_chain, bullet_chain, overall_simple_chain, handler
+    
+    load_dotenv()
+    
+    PUBLIC_KEY = os.getenv('PUBLIC_KEY')
+    SECRET_KEY = os.getenv('SECRET_KEY')
+    
+    handler = CallbackHandler(PUBLIC_KEY, SECRET_KEY)
+    
+    llm = ChatOpenAI(temperature=0.5, model_name="gpt-3.5-turbo")
 
-handler = CallbackHandler(PUBLIC_KEY, SECRET_KEY)
-
-llm = ChatOpenAI(temperature=0.5, model_name="gpt-3.5-turbo")
-
-
-refine_chain = load_summarize_chain(
+    
+    refine_chain = load_summarize_chain(
         llm,
         chain_type="refine",
         return_intermediate_steps=False,
         question_prompt=init_proompt,
-        refine_prompt=refine_prompt,
-)
-
-# intermediate_summary = refine_chain({"input_documents": docs}, return_only_outputs=True)
-
-bullet_chain = LLMChain(llm=llm, prompt=bullet_prompt, 
-                     output_key="bullet-summary"
-                    )
-
-overall_simple_chain = SimpleSequentialChain(chains=[refine_chain, bullet_chain],
-                                             verbose=True
-                                            )
+        refine_prompt=refine_prompt
+    )
+    
+    bullet_chain = LLMChain(llm=llm, prompt=bullet_prompt, output_key="bullet-summary")
+    
+    overall_simple_chain = SimpleSequentialChain(chains=[refine_chain, bullet_chain], verbose=True)
+    
 
 def generate_summary(docs):
+    global llm, refine_chain, bullet_chain, overall_simple_chain, handler
+    
+    if llm is None:
+        initialize_summarizer()
+        
     final_summary = overall_simple_chain({"input": docs}, return_only_outputs=True, callbacks=[handler])
     return final_summary['output']

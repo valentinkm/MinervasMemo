a9b07aa51f396122abbe898795d0d9440aade151
valentinkm <valentin.kriegmair@student.hu-berlin.de>
2023-11-07 18:58:06 +0100

    included call on minervasmemo main.py for summarization

--- git_operations.sh
+++ git_operations.sh
@@ -14,8 +14,18 @@ gh auth login --with-token <<< "$GH_TOKEN"
 # Change directory to the GitHub workspace where the code is checked out
 cd $GITHUB_WORKSPACE || exit
 
+# Fetch the latest main branch into a temporary branch
+git fetch origin main:temp-main
+
+# Checkout the temporary main branch
+git checkout temp-main
+
+# Create and checkout a new branch for the feature
+FEATURE_BRANCH="feature/add-summary-${GITHUB_SHA}"
+git checkout -b "$FEATURE_BRANCH"
+
 # Find changed VTT files
-files=$(git diff-tree --no-commit-id --name-only -r HEAD | grep 'docs_mr/.*\.vtt' | tr '\n' ' ')
+files=$(git diff --name-only HEAD $(git merge-base HEAD main) | grep 'docs_mr/.*\.vtt' | tr '\n' ' ')
 echo "Changed VTT files: $files"
 
 # List files before running script
@@ -32,18 +42,16 @@ token_info=$(cat docs_mr/*_token_info.txt | sed ':a;N;$!ba;s/\n/\\n/g')
 info="$model_info\\n$token_info"
 echo "Token and Cost Info: $info"
 
-# Commit changes and push
-BRANCH_NAME=$(git rev-parse --abbrev-ref HEAD)
+# Commit changes and push to the new branch
 git add docs_mr/*.md
 if git commit -m "Add cleaned up transcript and summary as markdown"; then
-  git push origin "$BRANCH_NAME"
+  git push origin "$FEATURE_BRANCH"
+  
   # Create Pull Request with the GitHub CLI
-  gh pr create --base main --head "$BRANCH_NAME" \
+  gh pr create --base main --head "$FEATURE_BRANCH" \
     --title "Add cleaned up transcript and summary" \
-    --body "$info"
-    --reviewer "$(git rev-parse --short HEAD)"
+    --body "$info" \
+    --reviewer "$(gh api /repos/:owner/:repo/collaborators | jq -r '.[] | .login')"
 else
   echo "No changes to commit"
 fi
-
-  

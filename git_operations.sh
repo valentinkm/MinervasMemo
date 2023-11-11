367e0a684334f35a0e3ec6e52ab6522070762e8c
valentinkm <valentin.kriegmair@student.hu-berlin.de>
2023-11-11 17:50:10 +0100

    handled newline for PR body

--- git_operations.sh
+++ git_operations.sh
@@ -85,8 +85,8 @@ FEATURE_BRANCH="add-summary-${sanitized_files[0]}"
 git checkout -b "$FEATURE_BRANCH"
 
 # Read token and cost information
-token_info=$(cat docs_mr/*_token_info.txt | sed ':a;N;$!ba;s/\n/\\n/g')
-info="$model_info\\n$token_info"
+token_info=$(cat docs_mr/*_token_info.txt | tr '\n' ' ' | sed 's/Total Tokens:/\\nTotal Tokens:/g')
+info="$model_info\n$token_info"
 echo "Token and Cost Info: $info"
 
 # Commit changes and push to the new branch

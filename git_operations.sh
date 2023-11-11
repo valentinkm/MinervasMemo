6062ceef38aaec6f27c8e32b82cf65f7e1d981a8
valentinkm <valentin.kriegmair@student.hu-berlin.de>
2023-11-11 17:09:10 +0100

    Handle Spaces in File Paths and Sanitize Filenames for Branch Naming

--- git_operations.sh
+++ git_operations.sh
@@ -8,6 +8,7 @@ if [ -z "$OPENAI_API_KEY" ] || [ -z "$GH_TOKEN" ] || [ -z "$PUBLIC_KEY" ] || [ -
   exit 1
 fi
 
+# Allow git to access the GitHub workspace
 git config --global --add safe.directory /github/workspace
 
 # Configure Git and GitHub CLI
@@ -18,47 +19,57 @@ gh auth login --with-token <<< "$GH_TOKEN"
 # Change directory to the GitHub workspace where the code is checked out
 export PYTHONPATH="/usr/src/app:$PYTHONPATH"
 
+ # Change directory to the GitHub workspace where the code is checked out
 cd $GITHUB_WORKSPACE || exit
 pwd
 echo "Current directory: $(pwd)"
 ls -alh
 
-echo "Contents of docs_mr/ directory:"
+# sanity checks
+echo "Contents of docs_mr/ directory:" 
 ls -alh docs_mr/
-
 echo "The current branch is:"
 git branch
-
 echo "GITHUB_REF: ${GITHUB_REF}"
 echo "GITHUB_SHA: ${GITHUB_SHA}"
 
+# Read file paths of changed VTT files in docs_mr/:
 echo "Checking for changed VTT files with SHA..."
-files=$(git diff --name-only "${GITHUB_SHA}^" "${GITHUB_SHA}" | grep 'docs_mr/.*\.vtt' | tr '\n' ' ')
+files=$(git diff --name-only "${GITHUB_SHA}^" "${GITHUB_SHA}" | grep 'docs_mr/.*\.vtt' | tr '\n' ':')
 echo "Changed VTT files: $files"
 
+# Convert the colon-separated string to an array
+IFS=":" read -ra file_array <<< "$files"
+
+ # Change directory to the app directory for executing main in minervasmemo.py
 cd /usr/src/app
-# list files in the current directory
-ls -l
+ls -l # sanity check for modules
 
 #chmod +x minervasmemo.py
 
-# Run Minerva's Memo script for summarization on each VTT file
-# New snippet that uses the $file variable that already contains the 'docs_mr/' path.
-for file in $files; do
-  echo "Processing $file"
-  python minervasmemo.py -i "/github/workspace/$file" --mode summarize >> minervasmemo.log 2>&1
+# Iterate over each changed VTT file, sanitize the file name, and process with minervasmemo.py
+for file in "${file_array[@]}"; do
+    # Sanitize the file name
+    sanitized_file=$(echo "$file" | sed 's/docs_mr\///' | sed 's/\.vtt//' | sed 's/[^a-zA-Z0-9]/-/g')
+    
+    echo "Processing $sanitized_file"
+    # Ensure to quote the path to handle spaces
+    python minervasmemo.py -i "/github/workspace/docs_mr/$sanitized_file.vtt" --mode summarize >> minervasmemo.log 2>&1
 done
 
-cat minervasmemo.log # Print the log file of minervasmemo.py
+cat minervasmemo.log # Print the log file of minervasmemo.py for debugging
 
-mv *summary*.md /github/workspace/docs_mr/
+# Move the cleaned up transcript and summary to docs_mr/ in the GitHub workspace
+mv *summary*.md /github/workspace/docs_mr/ 
 mv *token_info.txt /github/workspace/
 
+# Change directory to the GitHub workspace
 cd $GITHUB_WORKSPACE || exit
+
+# sanity checks
 pwd
-echo "Current directory: $(pwd)"
+echo "Current directory: $(pwd)" 
 ls -alh
-ls -l
 
 # Fetch the latest main branch into a temporary branch
 git fetch origin main:temp-main
@@ -66,8 +77,8 @@ git fetch origin main:temp-main
 # Checkout the temporary main branch
 git checkout temp-main
 
-# Create and checkout a new branch for the feature
-FEATURE_BRANCH="add-summary-$files"
+# Create and checkout a new branch for the Pull Request named after the first file
+FEATURE_BRANCH="add-summary-${sanitized_files[0]}"
 git checkout -b "$FEATURE_BRANCH"
 
 # Read token and cost information

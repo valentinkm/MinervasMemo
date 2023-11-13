3f014eecfbeda13b6cc6c8d31a8826e31486678c
valentinkm <valentin.kriegmair@student.hu-berlin.de>
2023-11-13 02:00:36 +0100

    refined prompts

--- map_reduce_prompts_minimal.py
+++ map_reduce_prompts_minimal.py
@@ -3,7 +3,7 @@ from langchain.prompts import PromptTemplate
 
 # Map
 map_template = """You are an excellent executive assistant. You get an automated meeting transcript with transcription error due to autmoatic voice recognition, please ignore those. \n
-Your task is to compress them as much as possible without loosing too much meaning. \n
+Your task is to summarize the transcript by condensing it without loosing meaning.\n
 Excerpts:`{text}`\n
 Comprehensive Meeting Notes:"""
 # map_prompt = PromptTemplate.from_template(map_template)
@@ -16,8 +16,8 @@ map_prompt_template = PromptTemplate (
 # Reduce
 reduce_template = """You are an excellent executive assistant. The following is a set of excerpts from meeting transcripts of a recent hybrid meeting: \n
 Excerpt:`{text}`\n
-Take these and in a stepwise manner condense the transcripts in one coherent document but WITHOUT loosing meaning. \n
-Thus step by step distill a comprehensive and cohesive summary of the meeting. \n
+Your task is to combine these into one coherent and concise document but WITHOUT loosing meaning. \n
+Step by step distill the text into a comprehensive and cohesive summary of the meeting. \n
 Meeting Summary:"""
 # reduce_prompt = PromptTemplate.from_template(reduce_template)
 combine_prompt_template = PromptTemplate(
@@ -52,10 +52,10 @@ combine_prompt_template2 = PromptTemplate(
 bullet_prompt = ChatPromptTemplate.from_template(
     """
     You are an excellent executive assistance. You are given a summary of a hybrid video conference:\n
-    Create a well structured concise summary from this in Markdown format.\n
+    Use this to create a well structured concise summary from this in Markdown format.\n
     Key Aspects to focus on in your summary are:\n
     - Clarity and Accuracy: Ensure the summary is clear and accurate, accounting for potential transcription errors.\n
-    - Highlight Key Elements: Emphasize decisions, action items, and dates.\n
+    - Highlight Key Points by emphasizing agreed upon decisions, action items, and dates.\n
     - Organize Efficiently: Use structured sections in the summary.\n
     - Include one section containing all agreed upon decisions and action items.\n
     - Keep the summary concise and to the point for efficient future reference of key elements.\n
@@ -67,15 +67,15 @@ bullet_prompt = ChatPromptTemplate.from_template(
 
 all_in_one_prompt = ChatPromptTemplate.from_template(
     """
-    You are an excellent executive assistance. You are given an machine-generated transcript of a hybrid video conference:\n
-    Create a well structured concise summary of this transcript in Markdown format.\n
+    You are an excellent executive assistance. You are given a summary of a hybrid video conference:\n
+    Use this to create a well structured concise summary from this in Markdown format.\n
     Key Aspects to focus on in your summary are:\n
-    Clarity and Accuracy: Ensure the summary is clear and accurate, accounting for potential transcription errors.\n
-    Highlight Key Elements: Emphasize decisions, action items, and dates.\n
-    Organize Efficiently: Use structured sections in the summary.\n 
-    Include one section containing all agreed upon decisions and action items.\n
-    Keep the summary concise and to the point for efficient future reference of key elements.\n
-    Markdown Formatting: Present the summary in Markdown format for readability and reference.\n
+    - Clarity and Accuracy: Ensure the summary is clear and accurate, accounting for potential transcription errors.\n
+    - Highlight Key Points by emphasizing agreed upon decisions, action items, and dates.\n
+    - Organize Efficiently: Use structured sections in the summary.\n
+    - Include one section containing all agreed upon decisions and action items.\n
+    - Keep the summary concise and to the point for efficient future reference of key elements.\n
+    - Markdown Formatting: Present the summary in Markdown format for readability and reference.\n
     Transcript: {docs}\n
     SUMMARY IN MARKDOWN:
     """

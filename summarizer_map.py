f1df92bc617ea3cf737f05873551645c49d6a64b
valentinkm <valentin.kriegmair@student.hu-berlin.de>
2023-11-14 17:55:23 +0100

    update token limits

--- summarizer_map.py
+++ summarizer_map.py
@@ -57,7 +57,7 @@ def initialize_summarizer():
         map_prompt=map_prompt_template,
         combine_prompt=combine_prompt_template,
         verbose=False,
-        token_max=8000
+        token_max=4000
     )
 
     summary_chain2 = load_summarize_chain (
@@ -66,7 +66,7 @@ def initialize_summarizer():
     map_prompt=map_prompt_template2,
     combine_prompt=combine_prompt_template2,
     verbose=False,
-    token_max=8000
+    token_max=4000
     )
     bullet_chain = LLMChain(llm=llm_final, prompt=bullet_prompt, output_key="bullet-summary")
     all_in_one_chain = LLMChain(llm=llm_all_in_one, prompt=all_in_one_prompt, output_key="all-in-one-summary")
@@ -84,7 +84,7 @@ def generate_summary_map(docs, token_count_transcript):
     token_info_final = {'Total Tokens': 0, 'Prompt Tokens': 0, 'Completion Tokens': 0, 'Total Cost (USD)': 0.0}
     aggregated_token_info = {'Total Tokens': 0, 'Total Cost (USD)': 0.0} 
 
-    if token_count_transcript < 100000:
+    if token_count_transcript < 4000:
         print("Token count below context window. Running single-prompt summary chain...\nModel: ", os.getenv('OPENAI_GPT_ALL_IN_ONE'))
 
         with get_openai_callback() as cb:
